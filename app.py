41dcd089001e547d8f548ad47355fb4604049c87
Yash Secondary <YashSec@Yashs-MacBook-Air.local>
2021-10-06 23:48:49 -0700

    tomorrow API weather forecast project

---
+++ app.py
@@ -0,0 +1,226 @@
+import requests
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json
+import dateutil.parser
+
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+weather_codes={}
+
+weather_codes[3002] = ['Strong Wind','strong_wind.svg']
+weather_codes[3000] = ['Light Wind','light_wind.svg']
+weather_codes[3001] = ['Wind','wind.svg']
+weather_codes[4201] = ['Heavy Rain','rain_heavy.svg']
+
+weather_codes[1000] = ['Clear Day','clear_day.svg']
+# weather_codes[1000] = ['Clear Night','clear_night.svg']
+
+weather_codes[1001] = ['Cloudy','cloudy.svg']
+weather_codes[4000] = ['Drizzle','drizzle.svg']
+weather_codes[2100] = ['Light Fog','fog_light.svg']
+weather_codes[2000] = ['Fog','fog.svg']
+weather_codes[6000] = ['Freezing Drizzle','freezing_drizzle.svg']
+weather_codes[6201] = ['Heavy Freezing Rain','freezing_rain_heavy.svg']
+weather_codes[6200] = ['Light Freezing Rain','freezing_rain_light.svg']
+weather_codes[6001] = ['Freezing Rain','freezing_rain.svg']
+weather_codes[5001] = ['Flurries','flurries.svg']
+weather_codes[7101] = ['Heavy Ice Pellets','ice_pellets_heavy.svg']
+weather_codes[7102] = ['Light Ice Pellets','ice_pellets_light.svg']
+weather_codes[7000] = ['Ice Pellets','ice_pellets.svg']
+
+weather_codes[1100] = ['Mostly Clear','mostly_clear_day.svg']
+# weather_codes[1100] = ['Mostly Clear','mostly_clear_night.svg']
+
+weather_codes[1102] = ['Mostly Cloudy','mostly_cloudy.svg']
+
+weather_codes[1101] = ['Partly Cloudy','partly_cloudy_day.svg']
+# weather_codes[1101] = ['Partly Cloudy','partly_cloud_night.svg']
+
+weather_codes[4200] = ['Light Rain','rain_light.svg']
+weather_codes[4001] = ['Rain','rain.svg']
+
+weather_codes[5101] = ['Heavy Snow','snow_heavy.svg']
+weather_codes[5100] = ['Light Snow','snow_light.svg']
+weather_codes[5000] = ['Snow','snow.svg']
+weather_codes[8000] = ['Thunderstorm','tstorm.svg']
+
+@app.route('/', methods=['GET','POST'])
+def weather_dashboard():
+    return render_template('home.html')
+
+@app.route('/fetch_weather_data_using_current_location', methods=['GET'])
+def fetch_weather_data_using_current_location():
+    latitude = request.args.get('lat');
+    longitude = request.args.get('lon');
+    address = request.args.get('address');
+    weather_current = get_weather_current(str(latitude),str(longitude))
+    weather_forecast = get_weather_forecast(str(latitude),str(longitude))
+    hourly_weather = get_hourly_data(str(latitude),str(longitude))
+    weather_current = weather_current['data']['timelines'][0]['intervals']
+    weather_forecast = weather_forecast['data']['timelines'][0]['intervals']
+    weather_forecast_hr = hourly_weather['data']['timelines'][0]['intervals']
+
+    for forecast in weather_forecast:
+        forecast['Date'] = dateutil.parser.isoparse(forecast['startTime']).strftime('%A, %d %B %Y')
+
+    data={}
+    data['current'] = weather_current
+    data['forecast'] = weather_forecast
+    data['hourly'] = weather_forecast_hr
+    data['address'] = address
+    data['weather_codes'] = weather_codes
+
+    return jsonify(data)
+    
+
+
+@app.route('/fetch_weather_data', methods=['GET'])
+def fetch_weather_data():
+    street = request.args.get('street')
+    u_street = street.replace(" ", "+")
+    city = request.args.get('city')
+    u_city = city.replace(" ", "+")
+    state = request.args.get('state')
+    address = u_street + '+' + u_city + '+' + state
+
+    geocoding_api = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=AIzaSyA69ez0zZevTG9TC3lfhgtXecbmB0JAgG8"
+    response = requests.get(geocoding_api)
+    response_info = json.loads(response.text)
+    latitude = response_info['results'][0]['geometry']['location']['lat']
+    longitude = response_info['results'][0]['geometry']['location']['lng']
+
+    weather_current = get_weather_current(str(latitude),str(longitude))
+    weather_forecast = get_weather_forecast(str(latitude),str(longitude))
+    hourly_weather = get_hourly_data(str(latitude),str(longitude))
+    weather_current = weather_current['data']['timelines'][0]['intervals']
+    weather_forecast = weather_forecast['data']['timelines'][0]['intervals']
+    weather_forecast_hr = hourly_weather['data']['timelines'][0]['intervals']
+
+    for forecast in weather_forecast:
+        forecast['Date'] = dateutil.parser.isoparse(forecast['startTime']).strftime('%A, %d %B %Y')
+
+    data={}
+    data['current'] = weather_current
+    data['forecast'] = weather_forecast
+    data['hourly'] = weather_forecast_hr
+    data['address'] = street + ' ' + city + ', ' + state + ', ' + 'US'
+    data['weather_codes'] = weather_codes
+
+    return jsonify(data)
+
+@app.route('/results', methods=['POST'])
+def render_results():
+    cuurentLocationEnabled = 'location' in request.form
+
+    latitude = ''
+    longitude = ''
+    address = ''
+    if(cuurentLocationEnabled == False):
+        street = request.form['street'].replace(" ", "+")
+        city = request.form['city'].replace(" ", "+")
+        state = request.form['state']
+        address = street + '+' + city + '+' + state
+    
+        geocoding_api = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=AIzaSyA69ez0zZevTG9TC3lfhgtXecbmB0JAgG8"
+        response = requests.get(geocoding_api)
+        response_info = json.loads(response.text)
+        latitude = response_info['results'][0]['geometry']['location']['lat']
+        longitude = response_info['results'][0]['geometry']['location']['lng']
+
+        address = request.form['street'] + ' ' + request.form['city'] + ', ' + state + ', ' + 'US'
+    else:
+        location = request.form['location']
+        location = json.loads(location)
+        city = location['city']
+        state = location['region']
+        postal = location['postal']
+        country = location['country']
+
+        address = city + ', ' + state +' ' +postal + ', ' + country
+
+        latitude = location['loc'].split(',')[0]
+        longitude = location['loc'].split(',')[0]
+    
+    weather_current = get_weather_current(str(latitude),str(longitude))
+    weather_forecast = get_weather_forecast(str(latitude),str(longitude))
+
+    weather_current = weather_current['data']['timelines'][0]['intervals']
+    weather_forecast = weather_forecast['data']['timelines'][0]['intervals']
+
+    for forecast in weather_forecast:
+        forecast['Date'] = dateutil.parser.isoparse(forecast['startTime']).strftime('%A, %d %B %Y')
+
+    # # return redirect(url_for('weather_dashboard', weather=weather),code=307)
+    weather_forecast_obj = json.dumps(weather_forecast)
+
+    return render_template('home.html', weather_current = weather_current, weather_forecast = weather_forecast, weather_forecast_obj = weather_forecast_obj,address=address, weather_codes=weather_codes, latitude=latitude, longitude=longitude)
+
+
+def get_weather_current(lat,lng):
+    # api_key = 'HKGMSz75AUwxbd6hbpBRYk5cp5r5GIpM'
+    # api_key = 'OoqJoSHqyZNGoATKMnYK3YWfdTUKIWB1'
+    # api_key = 'izOib0sas1iHtJoPXsiUZz3DYcOl9KIw'
+    # api_key = 'IKCaPz38UVKyCpkIrshm6DqERLg6AifD'
+    api_key = 'qCQKOmVhbQy19Jo7esiUyC1LlcRzc4a4'
+
+    api_url = "https://api.tomorrow.io/v4/timelines?location="+lat+","+lng+"&fields=temperature,temperatureApparent,temperatureMin,temperatureMax,windSpeed,windDirection,humidity,pressureSeaLevel,uvIndex,weatherCode,precipitationProbability,precipitationType,visibility,cloudCover&timesteps=current&units=metric&timezone=America/Los_Angeles&apikey="+ api_key
+    r = requests.get(api_url)
+    return r.json()
+
+def get_weather_forecast(lat,lng):
+    # api_key = 'HKGMSz75AUwxbd6hbpBRYk5cp5r5GIpM'
+    # api_key = 'OoqJoSHqyZNGoATKMnYK3YWfdTUKIWB1'
+    # api_key = 'izOib0sas1iHtJoPXsiUZz3DYcOl9KIw'
+    # api_key = 'IKCaPz38UVKyCpkIrshm6DqERLg6AifD'
+    api_key = 'qCQKOmVhbQy19Jo7esiUyC1LlcRzc4a4'
+
+
+    api_url = "https://api.tomorrow.io/v4/timelines?location="+lat+","+lng+"&fields=temperature,temperatureApparent,temperatureMin,temperatureMax,windSpeed,windDirection,humidity,pressureSeaLevel,uvIndex,weatherCode,precipitationProbability,precipitationType,sunriseTime,sunsetTime,visibility,moonPhase,cloudCover&timesteps=1d&units=metric&timezone=America/Los_Angeles&apikey=" + api_key
+    r = requests.get(api_url)
+    return r.json()
+
+def get_hourly_data(lat,lng):
+    # api_key = 'HKGMSz75AUwxbd6hbpBRYk5cp5r5GIpM'
+    # api_key = 'OoqJoSHqyZNGoATKMnYK3YWfdTUKIWB1'
+    # api_key = 'izOib0sas1iHtJoPXsiUZz3DYcOl9KIw'
+    # api_key = 'IKCaPz38UVKyCpkIrshm6DqERLg6AifD'
+    api_key = 'qCQKOmVhbQy19Jo7esiUyC1LlcRzc4a4'
+    
+    api_url = "https://api.tomorrow.io/v4/timelines?location="+lat+","+lng+"&fields=temperature,temperatureApparent,temperatureMin,temperatureMax,windSpeed,windDirection,humidity,pressureSeaLevel,uvIndex,weatherCode,precipitationProbability,precipitationType,visibility,cloudCover&timesteps=1h&units=metric&timezone=America/Los_Angeles&apikey=" + api_key
+    r = requests.get(api_url)
+    return r.json()
+
+@app.route('/result/today', methods=['POST','GET'])
+def getDayData():
+    data = request.get_json()
+
+    weather = json.loads(data['allWeather'])
+    
+    day_data={}
+
+    for detail in weather:
+        if detail['Date'] == data['Date']:
+            day_data={
+            'weather' : weather_codes[detail['values']['weatherCode']][0],
+            'weatherImg' : weather_codes[detail['values']['weatherCode']][1],
+            'date' : detail['Date'],
+            'humidity' : detail['values']['humidity'],
+            'precipitationProbability' : detail['values']['precipitationProbability'],
+            'precipitationType' : detail['values']['precipitationType'],
+            'sunriseTime' : detail['values']['sunriseTime'],
+            'sunsetTime' : detail['values']['sunsetTime'],
+            'temperatureMax' : detail['values']['temperatureMax'],
+            'temperatureMin' : detail['values']['temperatureMin'],
+            'visibility' : detail['values']['visibility'],
+            'windSpeed' : detail['values']['windSpeed']
+            }
+            break
+    # print(day_data)
+    return render_template('home.html', day_data = day_data)
+
+
+if __name__== '__main__':
+    app.run()
+
---
+++ static/js/charts.js
@@ -0,0 +1,533 @@
+function Meteogram(hourlyData, container) {
+    this.symbols = [];
+    this.humidities = [];
+    this.winds = [];
+    this.temperatures = [];
+    this.pressures = [];
+  
+    // Initialize
+    this.hourlyData = hourlyData;
+    this.container = container;
+  
+    // Run
+    this.parseYrData();
+  }
+  
+  /**
+  * Mapping of the symbol code in yr.no's API to the icons in their public
+  * GitHub repo, as well as the text used in the tooltip.
+  *
+  * https://api.met.no/weatherapi/weathericon/2.0/documentation
+  */
+  Meteogram.dictionary = {
+    clearsky: {
+        symbol: '01',
+        text: 'Clear sky'
+    },
+    fair: {
+        symbol: '02',
+        text: 'Fair'
+    },
+    partlycloudy: {
+        symbol: '03',
+        text: 'Partly cloudy'
+    },
+    cloudy: {
+        symbol: '04',
+        text: 'Cloudy'
+    },
+    lightrainshowers: {
+        symbol: '40',
+        text: 'Light rain showers'
+    },
+    rainshowers: {
+        symbol: '05',
+        text: 'Rain showers'
+    },
+    heavyrainshowers: {
+        symbol: '41',
+        text: 'Heavy rain showers'
+    },
+    lightrainshowersandthunder: {
+        symbol: '24',
+        text: 'Light rain showers and thunder'
+    },
+    rainshowersandthunder: {
+        symbol: '06',
+        text: 'Rain showers and thunder'
+    },
+    heavyrainshowersandthunder: {
+        symbol: '25',
+        text: 'Heavy rain showers and thunder'
+    },
+    lightsleetshowers: {
+        symbol: '42',
+        text: 'Light sleet showers'
+    },
+    sleetshowers: {
+        symbol: '07',
+        text: 'Sleet showers'
+    },
+    heavysleetshowers: {
+        symbol: '43',
+        text: 'Heavy sleet showers'
+    },
+    lightsleetshowersandthunder: {
+        symbol: '26',
+        text: 'Light sleet showers and thunder'
+    },
+    sleetshowersandthunder: {
+        symbol: '20',
+        text: 'Sleet showers and thunder'
+    },
+    heavysleetshowersandthunder: {
+        symbol: '27',
+        text: 'Heavy sleet showers and thunder'
+    },
+    lightsnowshowers: {
+        symbol: '44',
+        text: 'Light snow showers'
+    },
+    snowshowers: {
+        symbol: '08',
+        text: 'Snow showers'
+    },
+    heavysnowshowers: {
+        symbol: '45',
+        text: 'Heavy show showers'
+    },
+    lightsnowshowersandthunder: {
+        symbol: '28',
+        text: 'Light snow showers and thunder'
+    },
+    snowshowersandthunder: {
+        symbol: '21',
+        text: 'Snow showers and thunder'
+    },
+    heavysnowshowersandthunder: {
+        symbol: '29',
+        text: 'Heavy snow showers and thunder'
+    },
+    lightrain: {
+        symbol: '46',
+        text: 'Light rain'
+    },
+    rain: {
+        symbol: '09',
+        text: 'Rain'
+    },
+    heavyrain: {
+        symbol: '10',
+        text: 'Heavy rain'
+    },
+    lightrainandthunder: {
+        symbol: '30',
+        text: 'Light rain and thunder'
+    },
+    rainandthunder: {
+        symbol: '22',
+        text: 'Rain and thunder'
+    },
+    heavyrainandthunder: {
+        symbol: '11',
+        text: 'Heavy rain and thunder'
+    },
+    lightsleet: {
+        symbol: '47',
+        text: 'Light sleet'
+    },
+    sleet: {
+        symbol: '12',
+        text: 'Sleet'
+    },
+    heavysleet: {
+        symbol: '48',
+        text: 'Heavy sleet'
+    },
+    lightsleetandthunder: {
+        symbol: '31',
+        text: 'Light sleet and thunder'
+    },
+    sleetandthunder: {
+        symbol: '23',
+        text: 'Sleet and thunder'
+    },
+    heavysleetandthunder: {
+        symbol: '32',
+        text: 'Heavy sleet and thunder'
+    },
+    lightsnow: {
+        symbol: '49',
+        text: 'Light snow'
+    },
+    snow: {
+        symbol: '13',
+        text: 'Snow'
+    },
+    heavysnow: {
+        symbol: '50',
+        text: 'Heavy snow'
+    },
+    lightsnowandthunder: {
+        symbol: '33',
+        text: 'Light snow and thunder'
+    },
+    snowandthunder: {
+        symbol: '14',
+        text: 'Snow and thunder'
+    },
+    heavysnowandthunder: {
+        symbol: '34',
+        text: 'Heavy snow and thunder'
+    },
+    fog: {
+        symbol: '15',
+        text: 'Fog'
+    }
+  };
+  
+  /**
+  * Draw blocks around wind arrows, below the plot area
+  */
+  Meteogram.prototype.drawBlocksForWindArrows = function (chart) {
+    const xAxis = chart.xAxis[0];
+  
+    for (
+        let pos = xAxis.min, max = xAxis.max, i = 0;
+        pos <= max + 36e5; pos += 36e5,
+        i += 1
+    ) {
+  
+        // Get the X position
+        const isLast = pos === max + 36e5,
+            x = Math.round(xAxis.toPixels(pos)) + (isLast ? 0.5 : -0.5);
+  
+        // Draw the vertical dividers and ticks
+        const isLong = this.resolution > 36e5 ?
+            pos % this.resolution === 0 :
+            i % 2 === 0;
+  
+        chart.renderer
+            .path([
+                'M', x, chart.plotTop + chart.plotHeight + (isLong ? 0 : 28),
+                'L', x, chart.plotTop + chart.plotHeight + 32,
+                'Z'
+            ])
+            .attr({
+                stroke: chart.options.chart.plotBorderColor,
+                'stroke-width': 1
+            })
+            .add();
+    }
+  
+    // Center items in block
+    chart.get('windbarbs').markerGroup.attr({
+        translateX: chart.get('windbarbs').markerGroup.translateX + 8
+    });
+  
+  };
+  
+  /**
+  * Build and return the Highcharts options structure
+  */
+  Meteogram.prototype.getChartOptions = function () {
+    return {
+        chart: {
+            renderTo: this.container,
+            marginBottom: 70,
+            marginRight: 40,
+            marginTop: 50,
+            plotBorderWidth: 1,
+            height: 310,
+            alignTicks: false,
+            scrollablePlotArea: {
+                minWidth: 720
+            }
+        },
+  
+        defs: {
+            patterns: [{
+                id: 'precipitation-error',
+                path: {
+                    d: [
+                        'M', 3.3, 0, 'L', -6.7, 10,
+                        'M', 6.7, 0, 'L', -3.3, 10,
+                        'M', 10, 0, 'L', 0, 10,
+                        'M', 13.3, 0, 'L', 3.3, 10,
+                        'M', 16.7, 0, 'L', 6.7, 10
+                    ].join(' '),
+                    stroke: '#68CFE8',
+                    strokeWidth: 1
+                }
+            }]
+        },
+  
+        title: {
+            text: 'Hourly Weather (For Next 5 Days)',
+            align: 'center',
+            style: {
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis'
+            }
+        },
+  
+        credits: {
+            text: 'Forecast',
+            href: '#',
+            position: {
+                x: -40
+            }
+        },
+  
+        tooltip: {
+            shared: true,
+            useHTML: true,
+            headerFormat:
+                '<small>{point.timestamp:%A, %b %e, %H:%M}</small><br>'
+        },
+  
+        xAxis: [{ // Bottom X axis
+            type: 'datetime',
+            tickInterval: 2 * 36e5, // two hours
+            minorTickInterval: 36e5, // one hour
+            tickLength: 0,
+            gridLineWidth: 1,
+            gridLineColor: 'rgba(128, 128, 128, 0.1)',
+            startOnTick: false,
+            endOnTick: false,
+            minPadding: 0,
+            maxPadding: 0,
+            offset: 30,
+            showLastLabel: true,
+            labels: {
+                format: '{value:%H}'
+            },
+            crosshair: true
+        }, { // Top X axis
+            linkedTo: 0,
+            type: 'datetime',
+            tickInterval: 24 * 3600 * 1000,
+            labels: {
+                format: '{value:<span style="font-size: 12px; font-weight: bold">%a</span> %b %e}',
+                align: 'left',
+                x: 3,
+                y: -5
+            },
+            opposite: true,
+            tickLength: 20,
+            gridLineWidth: 1
+        }],
+  
+        yAxis: [{ // temperature axis
+            title: {
+                text: null
+            },
+            labels: {
+                format: '{value}°',
+                style: {
+                    fontSize: '10px'
+                },
+                x: -3
+            },
+            plotLines: [{ // zero plane
+                value: 0,
+                color: '#BBBBBB',
+                width: 1,
+                zIndex: 2
+            }],
+            maxPadding: 0.3,
+            minRange: 8,
+            tickInterval: 1,
+            gridLineColor: 'rgba(128, 128, 128, 0.1)'
+  
+        }, { // humidity axis
+            title: {
+                text: null
+            },
+            labels: {
+                enabled: false
+            },
+            gridLineWidth: 0,
+            tickLength: 0,
+            minRange: 10,
+            min: 0
+  
+        }, { // Air pressure
+            allowDecimals: false,
+            title: { // Title on top of axis
+                text: 'inHg',
+                offset: 0,
+                align: 'high',
+                rotation: 0,
+                style: {
+                    fontSize: '10px',
+                    color: Highcharts.getOptions().colors[3]
+                },
+                textAlign: 'left',
+                x: 3
+            },
+            labels: {
+                style: {
+                    fontSize: '8px',
+                    color: Highcharts.getOptions().colors[3]
+                },
+                y: 2,
+                x: 3
+            },
+            gridLineWidth: 0,
+            opposite: true,
+            showLastLabel: false
+        }],
+  
+        legend: {
+            enabled: false
+        },
+  
+        plotOptions: {
+            series: {
+                pointPlacement: 'between'
+            }
+        },
+  
+  
+        series: [{
+            name: 'Temperature',
+            data: this.temperatures,
+            type: 'spline',
+            marker: {
+                enabled: false,
+                states: {
+                    hover: {
+                        enabled: true
+                    }
+                }
+            },
+            tooltip: {
+                pointFormat: '<span style="color:{point.color}">\u25CF</span> ' +
+                    '{series.name}: <b>{point.y}°F</b><br/>'
+            },
+            zIndex: 1,
+            color: '#FF3333',
+            negativeColor: '#48AFE8'
+        }, {
+            name: 'Humidity',
+            data: this.humidities,
+            type: 'column',
+            color: 'rgb(149, 205, 250)',
+            yAxis: 1,
+            groupPadding: 0,
+            pointPadding: 0,
+            grouping: false,
+            dataLabels: {
+                enabled: true,
+                filter: {
+                    operator: '>',
+                    property: 'y',
+                    value: 0
+                },
+                style: {
+                    fontSize: '8px',
+                    color: 'gray'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' %'
+            }
+        }, {
+            name: 'Air pressure',
+            color: Highcharts.getOptions().colors[3],
+            data: this.pressures,
+            marker: {
+                enabled: false
+            },
+            shadow: false,
+            tooltip: {
+                valueSuffix: ' inHg'
+            },
+            dashStyle: 'shortdot',
+            yAxis: 2
+        }, {
+            name: 'Wind',
+            type: 'windbarb',
+            id: 'windbarbs',
+            color: Highcharts.getOptions().colors[1],
+            lineWidth: 1.5,
+            data: this.winds,
+            vectorLength: 18,
+            yOffset: -15,
+            tooltip: {
+                valueSuffix: ' mph'
+            }
+        }]
+    };
+  };
+  
+  /**
+  * Post-process the chart from the callback function, the second argument
+  * Highcharts.Chart.
+  */
+  Meteogram.prototype.onChartLoad = function (chart) {
+    this.drawBlocksForWindArrows(chart);
+  };
+  
+  /**
+  * Create the chart. This function is called async when the data file is loaded
+  * and parsed.
+  */
+  Meteogram.prototype.createChart = function () {
+    this.chart = new Highcharts.Chart(this.getChartOptions(), chart => {
+        this.onChartLoad(chart);
+    });
+  };
+  
+  Meteogram.prototype.parseYrData = function () {
+  
+    let pointStart;
+
+    // Loop over hourly forecasts
+    this.hourlyData.forEach((node, i) => {
+        const x = Date.parse(node[0]),
+            to = x + 36e5;
+  
+        if (to > pointStart + 120 * 36e5) {
+            return;
+        }
+  
+        this.temperatures.push({
+            x,
+            y: node[1],
+        });
+  
+        this.humidities.push({
+            x,
+            y: node[2]
+        });
+  
+        if (i % 2 === 0) {
+            this.winds.push({
+                x,
+                value: node[3],
+                direction: node[4]
+            });
+        }
+  
+        this.pressures.push({
+            x,
+            y: node[5]
+        });
+  
+        if (i === 0) {
+            pointStart = (x + to) / 2;
+        }
+    });
+
+    console.log(this.temperatures);
+  
+    // Create the chart when the data is loaded
+    this.createChart();
+  };
+  // End of the Meteogram protype
+  
+  function renderMeteogramChart(hourlyData) {
+      window.meteogram = new Meteogram(hourlyData, 'meteogram-container');
+  }
